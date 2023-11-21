6075dfe567ca6e95c572becdac21edfa6c637f17
u31511 <u31511@P31511>
2023-11-21 10:43:11 +1000

    Добавьте файлы проекта.

---
+++ Geometry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTask
+{
+    public class Geometry
+    {
+        static public void RectangleArea(int x)
+        {
+            int a = 3;
+            int b = 5;
+            int expected = a * b;
+        }
+    }
+}
